e72aef2a30467d4b9b33016cbfb30b0e073aa6d7
Kris Stange <kris.stange2015@gmail.com>
2016-04-05 15:27:24 -0700

    factor with new directory app.js routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  // console.log('we are in users.js');
-  // res.send('respond with a resource');
+  console.log('we are in users.js');
+  res.send('respond with a resource');
 });
-
+console
 module.exports = router;
